57467af6a467e130502deba792405087e761de85
Aroon Jawaid <aroonjawaid1@gmail.com>
2021-09-24 00:38:29 +0100

    Added quesitions

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 }
 
 /**
- * hides question 1 shows question2
+ * hides question 1 shows question 2
  */
 function question2() {
   document.getElementById("question1").style.display = "none";
@@ -15,11 +15,35 @@ function question2() {
 }
 
 /**
- * hides question 2 shows results
+ * hides question 2 shows question 3
+ */
+ function question3() {
+  document.getElementById("question2").style.display = "none";
+  document.getElementById("question3").style.display = "inline-block";
+}
+
+/**
+ * hides question 3 shows question 4
+ */
+ function question4() {
+  document.getElementById("question3").style.display = "none";
+  document.getElementById("question4").style.display = "inline-block";
+}
+
+/**
+ * hides question 3 shows question 4
+ */
+ function question5() {
+  document.getElementById("question4").style.display = "none";
+  document.getElementById("question5").style.display = "inline-block";
+}
+
+/**
+ * hides question  shows results
  */
 function results() {
   calculateResults()
-  document.getElementById("question2").style.display = "none";
+  document.getElementById("question5").style.display = "none";
   document.getElementById("results").style.display = "inline-block";
 }
 
@@ -46,18 +70,45 @@ function calculateResults() {
       }
   }
 
-  let total = Number(q1Value) + Number(q2Value);
+  const radios3 = document.querySelectorAll('input[name="quest3"]');
+  let q3Value;
+  for (const rb of radios3) {
+      if (rb.checked) {
+          q3Value = rb.value;
+          break;
+      }
+  }
+
+  const radios4 = document.querySelectorAll('input[name="quest4"]');
+  let q4Value;
+  for (const rb of radios4) {
+      if (rb.checked) {
+          q4Value = rb.value;
+          break;
+      }
+  }
+
+  const radios5 = document.querySelectorAll('input[name="quest5"]');
+  let q5Value;
+  for (const rb of radios5) {
+      if (rb.checked) {
+          q4Value = rb.value;
+          break;
+      }
+  }
+
+  let total = Number(q1Value) + Number(q2Value) + Number(q3Value) + Number(q4Value) + Number(q5Value);
   result = total / 2;
   
-  if (result > 2) {
+  if (result > 6) {
     msg = "You answered mostly c you may have a kinesthetic learning style";
-  } else if (result >= 1.5) {
+  } else if (result >= 3) {
     msg = "You answered mostly b, you may have an auditory learning style";
   } else {
     msg = "You answered mostly a, you may have a visual learning style";
   }
 
-  document.getElementById("result").innerHTML = msg
+  document.getElementById("result").innerHTML = msg;
 
 }
 
